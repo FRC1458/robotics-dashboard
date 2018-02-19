95da258586969db8790ab1bc766f678dfce1ebb8
DanKouch <DanKouch@users.noreply.github.com>
2018-02-19 11:00:07 -0800

    Mapped more values

--- scripts/main.js
+++ scripts/main.js
@@ -27,8 +27,7 @@ $(document).ready(() => {
 	updateVoltage(10);
 
 	NetworkTables.addKeyListener("/SmartDashboard/Arcade", (key, val) => {
-		console.log("Arcade = "+val)
-		$("#tankArcadeIndicator").text(`Current Drive: ${val === "a" ? "Arcade" : "Tank"}`)
+		$("#driveSelect").value(`${val === "a" ? "Arcade" : "Tank"} Drive`)
 	}, true)
 });
 
@@ -37,31 +36,45 @@ Number.prototype.map = function (in_min, in_max, out_min, out_max) {
 	  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+
 function keyValueChanged(key, value, isNew){
+
 	if(statusCheckElements.indexOf(key) > -1){
 		setStatusCheckElement(key, value);
 		return;
 	}
+
 	if(key == "voltage") {
 		updateVoltage(value);
 		return;
 	}
 
-	// By default, get an element with the ID of the key and set its text to the value.
-	//$("#"+key).text(value);
+	if(key == "hookLength"){
+		updateHookLength(value);
+		return;
+	}
+
+	if(key == "grabberLength"){
+		updateGrabberLengh(value);
+		return;
+	}
+
+	if(key == "blockHeld"){
+		updateBlockHeld(value);
+		return;
+	}
+
+	$("#"+key).text(value);
 }
 
-
-// Voltage metre
-function updateVoltage(voltage){	
-	var ofPotential = voltage.map(config.minVoltage, config.maxVoltage, 0, 100);
-
-	$("#voltageIndicatorBar").css("margin-left", ofPotential+"%")
-	$("#voltage").text(voltage)
+const statusCheckElements = [""]
+function setStatusCheckElement(elementID, status, err) {
+	$("#"+elementID).removeClass().addClass("glyphicon")
+		.addClass(status ? "glyphicon-ok" : (err) ? "glyphicon-flag" : "glyphicon-remove")
+		.addClass(status ? "text-success" : "text-danger")
+		.text(err);
 }
 
-
-// Robot diagram
 function updateHookLength(hookLengthPrecentage){
 	$("#robotHook").css("margin-top", hookLengthPrecentage.map(0, 100, config.minHookPrecentage, config.maxHookPrecentage)+"%");
 }
@@ -74,22 +87,16 @@ function updateBlockHeld(value){
 	$("#robotCube").css("display", value ? "inline" : "none");
 }
 
-// Type status check ids in  which have associated keys
-const statusCheckElements = [""]
-// Status items
-function setStatusCheckElement(elementID, status, err) {
-	$("#"+elementID).removeClass().addClass("glyphicon")
-		.addClass(status ? "glyphicon-ok" : (err) ? "glyphicon-flag" : "glyphicon-remove")
-		.addClass(status ? "text-success" : "text-danger")
-		.text(err);
+function driveSelectChanged() {
+	var newValue = ($("#driveSelect") == "Tank Drive" ? "t" : "a")
+    NetworkTables.putValue("SmartDashboard/Arcade", newValue);
 }
 
-function tank() {
-    NetworkTables.putValue("SmartDashboard/Arcade", "t")
-}
+function updateVoltage(voltage){	
+	var ofPotential = voltage.map(config.minVoltage, config.maxVoltage, 0, 100);
 
-function arcade() {
-	NetworkTables.putValue("SmartDashboard/Arcade", "a")
+	$("#voltageIndicatorBar").css("margin-left", ofPotential+"%")
+	$("#voltage").text(voltage)
 }
 
 function refreshImage() {
